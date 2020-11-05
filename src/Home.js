a728f8382c75f0c75f448954e587a81af0497e18
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-06 00:18:45 +0300

    add getPlanets functionality

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import './Home.css';
 import Planet from './Planet';
 import axios from 'axios';
 import Spinner from './Spinner';
+import PlanetContext from './context/Planet/planetContext';
 
 function Home() {
-  const [planets, setPlanets] = useState([]);
+  // const [planets, setPlanets] = useState([]);
+  const planetContext = useContext(PlanetContext);
+  const { getPlanets, planets } = planetContext;
 
   useEffect(() => {
-    async function fetchData() {
-      let i = 1;
-      let results = [];
-      while (true) {
-        try {
-          const response = await axios.get(
-            `https://swapi.dev/api/planets/?page=${i}`
-          );
-          results = results.concat(response.data.results);
-          i++;
-        } catch (error) {
-          console.log(error);
-          break;
-        }
-      }
-
-      //Filter unknown planets
-      const filteredPlanets = results.filter(
-        (result) => result.name !== 'unknown'
-      );
-      setPlanets(filteredPlanets);
-    }
-    fetchData();
+    getPlanets();
   }, []);
 
   // Cleanup
